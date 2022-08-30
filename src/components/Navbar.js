baebd1a58f47adea7902943d88ca7b86cc149391
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-08-30 21:07:08 +0530

    Added BootStrap framework and added bootstrap navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+     <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#">DeltaX</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon" />
+  </button>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="#">Home</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Link</a>
+      </li>
+     
+      <li className="nav-item">
+        <a className="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form className="form-inline my-2 my-lg-0">
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+    </div>
+  )
+}
+
+export default Navbar
